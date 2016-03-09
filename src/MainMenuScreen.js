07927e4c32c81eb16739bfd3b3e5de7570d57c8a
dbrabera <dbrabera@gmail.com>
2016-03-09 23:54:53 +0100

    Improve Main Menu screen

--- src/MainMenuScreen.js
+++ src/MainMenuScreen.js
@@ -1,16 +1,36 @@
 function MainMenuScreen(screens) {
     this.screens = screens;
+
+    this._titleChars = [
+        'd888888P dP                      d888888P          dP         ',
+        '   88    88                         88             88         ',
+        '   88    88 88d888b. dP    dP       88    .d8888b. 88 .d8888b.',
+        '   88    88 88\'  `88 88    88       88    88\'  `88 88 88ooood8',
+        '   88    88 88    88 88.  .88       88    88.  .88 88 88.  ...',
+        '   dP    dP dP    dP `8888P88       dP    `88888P8 dP `88888P\'',
+        '                          .88                                 ',
+        '                      d8888P                                  '
+    ];
+
+    this._titleColors = [
+        '01111110 00                      01111110          00         ',
+        '   11    11                         11             11         ',
+        '   11    11 11011100 00    00       11    00111100 11 00111100',
+        '   22    22 220  022 22    22       22    220  022 22 22000022',
+        '   22    22 22    22 220  022       22    220  022 22 220  000',
+        '   10    10 10    10 01111111       10    01111110 10 01111110',
+        '                          011                                 ',
+        '                      111110                                  '
+    ];
 }
 
 MainMenuScreen.prototype.step = function(display, mouse) {
-    display.write(9, 18, 'd888888P dP                      d888888P          dP         ');
-    display.write(9, 19, '   88    88                         88             88         ');
-    display.write(9, 20, '   88    88 88d888b. dP    dP       88    .d8888b. 88 .d8888b.');
-    display.write(9, 21, '   88    88 88\'  `88 88    88       88    88\'  `88 88 88ooood8');
-    display.write(9, 22, '   88    88 88    88 88.  .88       88    88.  .88 88 88.  ...');
-    display.write(9, 23, '   dP    dP dP    dP `8888P88       dP    `88888P8 dP `88888P\'');
-    display.write(9, 24, '                          .88                                 ');
-    display.write(9, 25, '                      d8888P                                  ');
+    var colors = ['#df9726', '#fbf236', '#fffdc0'];
+    for (var y = 0; y < this._titleChars.length; y++) {
+        for (var x = 0; x < this._titleChars[y].length; x++) {
+            display.write(9 + x, 18 + y, this._titleChars[y][x], colors[this._titleColors[y][x]]);
+        }
+    }
 
     if (button(display, mouse, 32, 33, 'Start a new game')) {
         this.screens.push(new GameScreen(this.screens));
@@ -21,9 +41,15 @@ function button(display, mouse, x, y, label) {
     var selected = mouse.x >= x && mouse.x < x + label.length && mouse.y == y;
 
     if (selected) {
-        display.write(32, 33, label, '#000000', '#ffffff');
+        display.write(x - 1, y, ' ', '#000000', '#ffffff', 0.25);
+        display.write(x - 2, y, ' ', '#000000', '#ffffff', 0.5);
+
+        display.write(x, y, label, '#000000', '#ffffff');
+
+        display.write(x + label.length, y, ' ', '#000000', '#ffffff', 0.25);
+        display.write(x + label.length + 1, y, ' ', '#000000', '#ffffff', 0.5);
     } else {
-        display.write(32, 33, label, '#ffffff', '#000000');
+        display.write(x, y, label, '#9badb7', '#000000');
     }
 
     return selected && mouse.clicked;
